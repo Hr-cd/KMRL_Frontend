20696393e13a35f276d30f6541e2ba9d6bb6cf52
Hrushikesh Paraye <parayehrushikesh10@gmail.com>
2026-03-05 08:08:01 +0530

    Server Change!

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "kmrl-frontend.onrender.com",
     port: 8080,
     hmr: {
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react(), mode === "production" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
